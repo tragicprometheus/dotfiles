fdddba3870c8a650bc51ad9aea9fdb4a030ea7fa
Joe Trenshaw <joe@joetrenshaw.com>
2026-04-25 22:15:19 -0400

    add sioyek and zsh

---
+++ retrieve_dotfiles.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Basics
+cp ~/.zshrc ./zshrc
+cp ~/.config/git/config ./gitconfig
+
+# Sioyek Configuraion
+cp ~/.config/sioyek/keys_user.config ./sioyek/keys_user.config
+cp ~/.config/sioyek/prefs_user.config ./sioyek/prefs_user.config
