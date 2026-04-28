e92111c72f7862f5d72a0ee4e55e12e2de061b4f
Joe Trenshaw <joseph.trenshaw@geaerospace.com>
2026-04-28 12:17:52 -0400

    add logic for windows vs linux

--- init_dotfiles.sh
+++ init_dotfiles.sh
@@ -1,8 +1,27 @@
 #!/bin/bash
 
-cp ./zshrc ~/.zshrc
-cp ./gitconfig 	~/.config/git/config
+case "$OSTYPE" in
+  linux-gnu*)
+		echo "Linux"
+		zshrc_path="~/.zshrc"
+		gitconfig_path="~/.config/git/config"
+		sioyek_keys_config="~/.config/sioyek/keys_user.config"
+		sioyek_prefs_config="~/.config/sioyek/prefs_user.config"
+		;;
+  cygwin*|msys*|win32*)
+		echo "Windows (Cygwin/MSYS/Git Bash)"
+		# zshrc_path="~/.zshrc"
+		gitconfig_path="~/.gitconfig"
+		sioyek_keys_config="C:/ProgramData/sioyek/keys_user.config"
+		sioyek_prefs_config="C:/ProgramData/sioyek/prefs_user.config"
+		;;
+  *)
+		echo "Unknown OS: $OSTYPE"
+		;;
+esac
 
-# Sioyek Configuraion -- ~/.config/sioyek/
-cp ./sioyek/keys_user.config ~/.config/sioyek/keys_user.config
-cp ./sioyek/prefs_user.config ~/.config/sioyek/prefs_user.config
+cp ./zshrc $zshrc_path
+cp ./gitconfig $gitconfig_path
+# Sioyek Configuraion 
+cp ./sioyek/keys_user.config $sioyek_keys_config
+cp ./sioyek/prefs_user.config $sioyek_prefs_config
--- retrieve_dotfiles.sh
+++ retrieve_dotfiles.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
 
+case "$OSTYPE" in
+  linux-gnu*)
+		echo "Linux"
+		zshrc_path="~/.zshrc"
+		gitconfig_path="~/.config/git/config"
+		sioyek_keys_config="~/.config/sioyek/keys_user.config"
+		sioyek_prefs_config="~/.config/sioyek/prefs_user.config"
+		;;
+  cygwin*|msys*|win32*)
+		echo "Windows (Cygwin/MSYS/Git Bash)"
+		# zshrc_path="~/.zshrc"
+		gitconfig_path="~/.gitconfig"
+		sioyek_keys_config="C:/ProgramData/sioyek/keys_user.config"
+		sioyek_prefs_config="C:/ProgramData/sioyek/prefs_user.config"
+		;;
+  *)
+		echo "Unknown OS: $OSTYPE"
+		;;
+
+esac
 # Basics
-cp ~/.zshrc ./zshrc
-cp ~/.config/git/config ./gitconfig
+cp $zshrc_path ./zshrc
+cp $gitconfig_path ./gitconfig
 
 # Sioyek Configuraion
-cp ~/.config/sioyek/keys_user.config ./sioyek/keys_user.config
-cp ~/.config/sioyek/prefs_user.config ./sioyek/prefs_user.config
+cp $sioyek_keys_config ./sioyek/keys_user.config
+cp $sioyek_prefs_config ./sioyek/prefs_user.config
